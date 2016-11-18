3e6f569994b4f6bf034fe1e3c7bbe0a2eac7b37b
Ganusha93 <mkgjmadusanka@gmail.com>
2016-11-18 16:26:09 +0530

    commit 03

---
+++ logs/error.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-05-24 07:09:19 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-05-24	07:09:19	INFO	::1	Joomla FAILURE: 	Empty password not allowed
+2016-11-10	15:30:00	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-11	07:09:42	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-14	23:48:30	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-15	00:59:19	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-17	15:46:50	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-17	16:16:25	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2016-11-17	16:16:25	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
